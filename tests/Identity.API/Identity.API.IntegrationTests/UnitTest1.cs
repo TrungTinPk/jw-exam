0a0a4ab81f6c37064377579fdf335d622213746a
Kevin Trần <tintt.pk@gmail.com>
2021-07-20 21:13:13 +0700

    Create solution

---
+++ tests/Identity.API/Identity.API.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Identity.API.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
